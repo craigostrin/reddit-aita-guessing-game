41e253940abb96af06afce8cc48229ca7b592845
Craig Ostrin <craigostrin@gmail.com>
2023-11-24 23:44:26 +0100

    feat(content script): Count votes on r/AITA pages
    ⚠️ This only works properly on `old.reddit.com`

---
+++ src/tally-votes.ts
@@ -0,0 +1,39 @@
+import { Tally } from './types'
+
+const tally: Tally = {
+  YTA: 0,
+  NTA: 0,
+  ESH: 0,
+  NAH: 0,
+}
+
+const VOTES = Object.keys(tally) as (keyof typeof tally)[]
+
+console.log("Looks like you're on r/AmITheAsshole")
+console.log(`Let's count these votes: ${VOTES}`)
+
+//! This only works on Old Reddit (old.reddit.com)
+// new.reddit may have some dynamic loading based on scroll
+// sh.reddit streams its comments so the script runs too soon
+const nodes = document.querySelectorAll('.usertext-body > div > p')
+
+//TODO exclude sidebar
+const comments = Array.from(nodes, (node) => node.textContent)
+console.log(comments)
+const start = Date.now()
+console.log(start)
+
+comments.forEach((comment) => {
+  VOTES.forEach((key) => {
+    if (comment?.indexOf(key) !== -1) tally[key]++
+  })
+})
+
+const finish = Date.now()
+console.log(finish)
+
+console.log('The votes come to:')
+console.log(tally)
+console.log(`\nAITA vote tally took ${finish - start} ms.`)
+
+chrome.runtime.sendMessage({ tally })
---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type Tally = {
+  YTA: number
+  NTA: number
+  ESH: number
+  NAH: number
+}
+
+//* Main votes to count
+// YTA = You're the Asshole;
+// NTA = Not the A-hole (and the other person is);
+// ESH = Everyone Sucks here;
+// NAH = No A-holes here;
+
+//* Rare votes, not counted
+// YWBTA = You Would Be the Asshole;
+// YWNBTA = You Would Not be the Asshole (and the other person would);
+// INFO = Not Enough Info
