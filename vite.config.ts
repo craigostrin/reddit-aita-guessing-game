bb56b856535fe9a6c7daf0e819a52bbdc7949bff
Craig Ostrin <craigostrin@gmail.com>
2023-11-24 23:47:20 +0100

    fix(vite config): fixes "failed to construct websocket" error on Chrome Extensions settings page

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,12 @@ import manifest from './manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    crx({ manifest }),
-  ],
+  plugins: [react(), crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
 })
