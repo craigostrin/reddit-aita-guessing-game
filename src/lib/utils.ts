e8b1214d9fb3e1ae343fd8f5955f529dff8e4534
Craig Ostrin <craigostrin@gmail.com>
2023-12-07 19:29:25 +0100

    Add getActiveTabUrl() just in case

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,14 @@ export function getPostIdFromUrl(url: string): PostId {
   const i = split.indexOf('comments') + 1
   return split[i]
 }
+
+//* Only needed for Popup. Use 'location.href' in scripts
+// export async function getActiveTabUrl(): Promise<string> {
+//   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+//   if (!tab.url) {
+//     throw new Error('Error accessing current tab URL')
+//   }
+
+//   return tab.url
+// }
